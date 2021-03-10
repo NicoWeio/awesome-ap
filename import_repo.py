e4c9c15739aedb46ca6ae076127ca1ebd82ac462
Nicolai Weitkemper <nico.weio@gmail.com>
2021-03-10 13:35:18 +0100

    Versuch-Nummern: Erkennung verbessert, Duplikate vermieden

--- import_repo.py
+++ import_repo.py
@@ -9,7 +9,7 @@ def getLastCommit(repo):
     return sorted(dates)[-1]
 
 def getVersuchNummer(dirname):
-    s = re.search(r'[VD][._]?(\d{3})', dirname, re.IGNORECASE)
+    s = re.search(r'[VD]?[._]?(\d{3})', dirname, re.IGNORECASE)
     return int(s.group(1)) if s else None
 
 def import_repo(source, gh):
@@ -19,8 +19,8 @@ def import_repo(source, gh):
     contents = repo.get_contents(source.get('subdirectory', ''))
     dirsOfInterest = getDirsOfInterest(contents)
 
-    versuchNummern = [getVersuchNummer(d.name) for d in dirsOfInterest]
-    versuchNummern = [i for i in versuchNummern if i] # nicht erkannte Versuchs-Nummern entfernen
+    versuchNummern = set([getVersuchNummer(d.name) for d in dirsOfInterest])
+    versuchNummern.discard(None)
 
     source['contributors'] = list(repo.get_contributors())
     source['lastCommit'] = getLastCommit(repo)
