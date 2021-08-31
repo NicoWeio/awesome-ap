ef4a35df9f4581d842a6e7012d1150af622f62d5
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2021-08-31 15:04:23 +0200

    Discover-Skript in GraphQL umgeschrieben
    → schneller, keine Probleme mehr mit rate limits

--- specials/discover/main.py
+++ specials/discover/main.py
@@ -1,13 +1,76 @@
-import github
 from dotenv import load_dotenv
 import os
 from rich import print, progress
+import requests
 import yaml
-
 load_dotenv()
-# funktioniert auch ohne Token
 TOKEN = os.getenv('GITHUB_TOKEN') or os.getenv('INPUT_GITHUB_TOKEN')
-gh = github.Github(TOKEN)
+headers = {"Authorization": 'Bearer ' + TOKEN}
+
+
+def run_query(query):  # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        response = request.json()
+        if errors := response.get('errors'):
+            raise Exception(f"GraphQL reported error(s): {' +++ '.join(e.get('message', e) for e in errors)}")
+        return response
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+def query_repos_for_user(user):
+    query = """
+    {
+        user(login:"<<<USER>>>") {
+            followers(first: 100) {
+                nodes {
+                    ...dings
+                }
+            }
+            following(first: 10) {
+                nodes {
+                    ...dings
+                }
+            }
+        }
+    }
+    fragment dings on User {
+        login
+        repositories(isFork: false, first: 100) {
+            nodes {
+                nameWithOwner
+                stargazerCount
+            }
+        }
+    }
+    """.replace('<<<USER>>>', user)
+    try:
+        result = run_query(query)
+        followers = result['data']['user']['followers']['nodes']
+        following = result['data']['user']['following']['nodes']
+        all_users = followers + following
+        all_repos = [repo for user in all_users for repo in user['repositories']['nodes']]
+        print(f"{user}: {len(followers)} followers, {len(following)} following → {len(all_repos)} repos")
+        return all_repos
+    except Exception as e:
+        print(f'An error occured while querying {user}:', e)
+        return []
+
+
+def query_rate_limit():
+    query = """
+    {
+        rateLimit {
+            limit
+            remaining
+            resetAt
+        }
+    }
+    """
+    result = run_query(query)
+    return result['data']['rateLimit']
+
 
 with open('sources.yaml', 'r') as stream:
     sources = yaml.safe_load(stream)
@@ -15,54 +78,34 @@ with open('sources.yaml', 'r') as stream:
 with open('specials/discover/config.yaml', 'r') as stream:
     config = yaml.safe_load(stream)
 
+repos_from_sources = [source['name'] for source in sources]
+users_from_sources = sorted(set([source['name'].split('/')[0] for source in sources]), key=str.casefold)
 
-def unique_repos(repos):
-    seen = set()
-    return [seen.add(obj.full_name) or obj for obj in repos if obj.full_name not in seen]
+print(f"Rate limit before: {query_rate_limit()}")
 
+repos = [repo for user in progress.track(users_from_sources, description='Querying users…')
+         for repo in query_repos_for_user(user)]
 
-def unique_users(users):
-    seen = set()
-    return [seen.add(obj.name) or obj for obj in users if obj.name not in seen]
+print(f"Rate limit after: {query_rate_limit()}")
+
 
 def match_repo(repo):
     matches = [
-        any(match in repo.name.lower() for match in ['ap', 'fp', 'praktikum', 'protokolle']),
-        repo.full_name not in config.get('ignore', []),
+        # nicht ignoriert?
+        repo['nameWithOwner'] not in config.get('ignore', []),
+        # nicht bereits vorhanden?
+        not repo['nameWithOwner'] in repos_from_sources,
+        # Schlüsselwort im Namen?
+        any(match in repo['nameWithOwner'].split('/')[1].lower() for match in ['ap', 'fp', 'praktikum', 'protokolle']),
     ]
     return all(matches)
 
-source_repos = [source['name'] for source in sources]
-usernames = [repo.split('/')[0] for repo in source_repos]
 
-users = {username: gh.get_user(username) for username in progress.track(usernames, description='Getting known users…')}
-
-for username, user in progress.track(users.copy().items(), description='Finding users…'):
-    print(f"{user=}")
-    for follower in list(user.get_followers()):
-        print(f"{follower=}")
-        users.update({follower.login: follower})
-    for followee in list(user.get_following()):
-        print(f"{followee=}")
-        users.update({followee.login: followee})
-
-print(f"{users=}")
-print('–' * 10)
-
-all_new_candidates = []
-for user in progress.track(users.values(), description='Discovering repos…'):
-    print(f'→ {user.login}:')
-    repos = unique_repos(list(user.get_repos()) + list(user.get_starred()))
-    # repo_candidates = [repo for repo in repos if ]
-    repo_candidates = list(filter(match_repo, repos))
-    new_candidates = [repo for repo in repo_candidates if not repo.full_name in source_repos]
-    if new_candidates:
-        print('[green]' + '\n'.join(candidate.full_name for candidate in new_candidates) + '[/green]')
-        all_new_candidates.extend(new_candidates)
-    else:
-        print(f'{len(repos)} repo(s); {len(repo_candidates)} candidate(s); {len(new_candidates)} new')
-
-all_new_candidates_out = sorted(set([repo.full_name for repo in all_new_candidates]), key=str.casefold)
+all_candidates = list(filter(match_repo, repos))
+all_candidates_out = sorted(set([repo['nameWithOwner'] for repo in all_candidates]), key=str.casefold)
 
 with open('specials/discover/results.yaml', 'w') as outfile:
-    yaml.dump(all_new_candidates_out, outfile, default_flow_style=False)
+    yaml.dump(all_candidates_out, outfile, default_flow_style=False)
+
+print("Done! 🎉")
+print(f"{len(all_candidates_out)} repo candidates found")
