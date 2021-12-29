6c986079451e1086ca9346c32b41778a1f193e99
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2021-12-29 17:20:22 +0100

    verstecke Progress-Bar in GitHub Actions-Umgebung

--- console.py
+++ console.py
@@ -2,6 +2,8 @@ import os
 from rich.console import Console
 from rich.progress import track as _track
 
+GITHUB_ACTIONS = os.getenv('GITHUB_ACTIONS', 'False').lower() == 'true'
+
 
 def should_force_terminal():
     return True if any(os.getenv(key) for key in ['GITHUB_ACTIONS', 'FORCE_COLOR', 'PY_COLORS']) else None
@@ -22,7 +24,11 @@ warn = makeLogFn('yellow')
 error = makeLogFn('red bold')
 
 
-def track(iterable, **kwargs):
+def track_wrapper(iterable, **kwargs):
     kwargs.setdefault('transient', True)
     kwargs.setdefault('console', console)
     return _track(iterable, **kwargs)
+
+
+# GitHub Actions rendert die Progress-Bars nicht richtig, daher werden sie dort deaktiviert.
+track = (lambda iterable, **kwargs: iterable) if GITHUB_ACTIONS else track_wrapper
